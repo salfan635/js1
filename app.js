907075215bbf32972675ba98c0993e207ddbfc11
salfan635 <120503480+salfan635@users.noreply.github.com>
2023-02-14 19:54:37 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+'use strict'
+
+// const nam = prompt('Hello, what is you name?')
+// alert('hello ' + nam);
+
+
+// const y = 2023;
+// let youHB = +prompt('you HB');
+// let youY = y - youHB;
+// alert(youY);
+
+
+// const st = 4;
+// let stW = +prompt('mm');
+// let rez = st * stW;
+// alert(rez + ' mm');
+
+
+// const Pi = Math.PI;
+// let r = +prompt('radius(mm)');
+// let rez = Pi * r * 2;
+// alert(rez + ' mm^3')
+
+
+// let km = +prompt('distance between');
+// let hour = +prompt('estimated time');
+// let rez1 = km / hour;
+// let rez2 = Math.round(rez1)
+// alert('desired speed ' + rez2 + ' km/h')
+
+
+// const eu = 0.92;
+// let usdt = +prompt('how much d-s you have?');
+// let rez1 = eu * usdt;
+// let rez2 = Math.round(rez1)
+// alert(rez2 + ' eu')
+
+
+// const gb = 820;
+// let fl = +prompt('you volume');
+// let flGb = fl * 1024
+// let rez1 = flGb / 820;
+// let rez2 = Math.round(rez1);
+// alert(rez2)
+
+
+// let coAm = +prompt('amount of chocolate');
+// const coPrice = 5;
+// let co = coAm * coPrice;
+// let mon = +prompt('how much money do you have?')
+// let rez1 = mon / co;
+// let rez2 = Math.round(rez1);
+// let rezMon = mon % co;
+// alert(rez2 + ' chocolate ' + rezMon + ' money')
+
+
+// let n = +prompt('');
+// let nStr = String(n)
+// let nLen = nStr.length;
+// if(nLen >= 3 && nLen <= 3){
+//     let n1 = n / 100;
+//     let nm1 = Math.round(n1) 
+//     let no1 = n % 100;
+//     let n2 = no1 / 10;
+//     let nm2 = Math.round(n2)
+//     let nm3 = no1 % 10;
+//     let ns2 =  String(nm2)
+//     alert(nm3 + ns2 + nm1)
+// }else{
+//     alert('Вы ввели ' + nLen + '-значное число, а нужно было 3-значное')
+// }
+
+
+// let a = +prompt('num')
+// let a1 = a % 2;
+// (a1 == 0) && alert( 'parnoe' );
+// (a1 > 0) && alert( 'ne parnoe' );
+# js1
